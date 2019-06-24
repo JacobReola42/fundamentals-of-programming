8ea385314bcee1d128858d8b6331ea74ff4174cc
Jacob Reola <jacobreola@gmail.com>
2019-06-24 04:08:41 -0500

    modularized rm brute -f

--- checkbookBalancingV1/main.cpp
+++ checkbookBalancingV1/main.cpp
@@ -5,82 +5,119 @@
 
 const double SERVICE_CHARGE = 0.35;
 
-int main()
-{
+void startProgram() {
+   std::cout << "Checkbook balancing program \n";
+} 
 
-// declaring variables 
+float beginningBalance() {
+   float startingBalance,
+       balance;
+   std::cout << "Enter the beginning balance: ";
+   std::cin >> startingBalance;
+   return startingBalance;
+} 
 
-    double balance,
-            amount,
-            totalServiceCharge = 0;
-    std::string choice;
 
-// begins checkbook balancing program and displays menu
-// gets initial balance
-    std::cout << "Checkbook Balancing Program\n"; 
-    std::cout << "Enter the beginning balance: \n";
-    std::cin  >> balance;
-    std::cout << "" << std::endl;
-    std::cout << "Commands: \n";
-    std::cout << "C - process a check\n";
-    std::cout << "D - process a deposit\n";
-    std::cout << "E - end the program\n";
+void menuOption() {
+   std::cout << "\nCommands:\n";
+   std::cout << "C - process a check\n";
+   std::cout << "D - process a deposit\n";
+   std::cout << "E - end the program\n";
+} 
 
-// infinite while loop for transactions
-// gets transaction type
-    while(1){
-        std::cout << "Enter transaction type: ";
-        std::cin  >> choice;
-        amount = 0;
-        if(choice == "C"){
+bool processCheck (float& begBal, float& totalCharge) {
+    double rollingCharge;
 
-            // loops if input is illegal
+    std::cout << "Enter transaction amount: ";
+    std::cin  >> rollingCharge;
 
-            while (amount <= 0){
-                std::cout << "Enter transaction amount: ";
-                std::cin  >> amount;
-            }
+    if (rollingCharge <= 0) {
+        std::cout << "\nInvalid transaction amount.\n";
+        std::cout << "Canceling transaction.";
+        return 0;
+    } else {   
+        std::cout << "Processing check for $" << rollingCharge;
 
-            // process a check
+        begBal -= rollingCharge;
+        totalCharge += SERVICE_CHARGE; 
 
-            std::cout << "Processing check for $" << std::fixed <<  std::setprecision(2) << amount << std::endl;
-            balance -= amount;
-            totalServiceCharge += SERVICE_CHARGE;
-            std::cout << "Balance: $" << std::setprecision(2) << balance << std::endl;
-            std::cout << "Service charge: $" << SERVICE_CHARGE << " for a check" << std::endl;
-            std::cout << "Total service charges: $" << std::setprecision(2) << totalServiceCharge << std::endl;
-
-        }else if (choice == "D"){
-
-            // loops if input is illegal
-
-            while (amount <= 0){
-                std::cout << "Enter transaction amount: ";
-                std::cin  >> amount;
-            }
-
-            // process deposit
-
-            std::cout << "Processing deposit for $" << amount << std::endl;
-            balance += amount;
-            std::cout << "Balance: $" << balance << std::endl;
-            std::cout << "Total Service charges: $" << totalServiceCharge << std::endl;
-
-            // ending the program and displays ending balance
-
-        }else if (choice == "E"){
-            balance -= totalServiceCharge;
-            std::cout << "Processing end of month\n";
-            std::cout << "Final balance: $" << balance << std::endl;
-            break;
-        }else{
-            // notify if input is illegal
-            std::cout << "Not a valid command. Please choose from C, D, and E" << std::endl;
-        }
-        std::cout << "" << std::endl;
-
-// ends the while loop of transaction types   
-    }
-
+        std::cout << "\nBalance: $" << begBal;
+        std::cout << "\nService charge: $" << SERVICE_CHARGE << " for a check";
+        std::cout << "\nTotal service charges: $" << totalCharge;
+        std::cout << std::endl;
+        return 1;
+    }   
     return 0;
 }
+
+bool processDeposit(float& begBal, float& totalCharge) {
+   float rollingCharge;
+
+   std::cout << "Enter transaction amount: ";
+   std::cin  >> rollingCharge;
+
+   if (rollingCharge <= 0) {
+      std::cout << "\nInvalid transaction amount.\n";
+      std::cout << "Canceling transaction.";
+      return 0;
+   } 
+   else {
+      std::cout << "Processing check for $" << rollingCharge;
+
+      begBal += rollingCharge;
+
+      std::cout << "\nBalance: $" << begBal;
+      std::cout << "\nTotal service charges: $" << totalCharge;
+      std::cout << std::endl;
+      return 1;
+      }
+   return 0;
+}
+
+
+void transactionProcess(float& begBal, float& totalCharge) {
+   char userInput;
+
+   while(userInput != 'E') {
+     std::cout << "\nEnter transaction type:";
+     std::cin >> userInput;
+     if (userInput =='C') {
+        processCheck(begBal, totalCharge);
+     } 
+     else if (userInput == 'D') {
+        processDeposit(begBal, totalCharge);
+     }
+     else if (userInput == 'E') {
+        break;
+     } 
+     else {
+        std::cout << "ERROR!\n";
+     }  
+   }
+} 
+
+
+bool endProgram(float& begBal, float& totalCharge) {
+
+    begBal = (begBal - totalCharge);
+    std::cout << "Processing end of month\n";
+    std::cout << "Final balance: $" << begBal;
+    std::cout << std::endl;
+
+    return 1;
+}
+
+
+int main() {
+
+   float begBal,
+         totalCharge;
+
+   startProgram();
+   begBal = beginningBalance();
+   menuOption();
+   transactionProcess(begBal, totalCharge); // total charge gets processed in this func.
+   endProgram(begBal, totalCharge);
+
+   return 0;
+} 
